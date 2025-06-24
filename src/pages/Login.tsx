e9d878cc5bed3f48b8aca39e876c1a5ac6f4bd28
mutheer <112380638+mutheer@users.noreply.github.com>
2025-06-24 09:38:28 +0200

    Updated Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -353,7 +353,7 @@ const Login: React.FC = () => {
               </div>
 
               <div className="mt-4 text-sm text-center text-gray-600 bg-blue-50 p-3 rounded-md">
-                <p className="font-medium">Email: mudhirabu@gmail.com</p>
+                <p className="font-medium">Email: muthirabu@gmail.com</p>
                 <p className="font-medium">Password: admin123</p>
               </div>
             </div>
